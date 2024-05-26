05a72344e9221b7687bbde1e7880ab090b5514c2
Alexis Diaz <alexis.diaz@cua.uam.mx>
2024-05-25 20:07:41 -0600

    Creacion directorio Panel

---
+++ panel/pines.h
@@ -0,0 +1,16 @@
+#ifndef C_PINES_H
+#define C_PINES_H
+
+#define FIRST_GPIO 2
+#define BOTON_ONOFF 16
+#define BOTON_CONFIRMACION 17
+#define BOTON_PAUSA 18
+#define LED_ONOFF 19
+#define BOTON_MODO 20
+#define BOTON_TEMPERATURA 21
+#define BOTON_NIVEL 22
+#define LED_FRIA 26
+#define LED_TIBIA 27
+#define LED_CALIENTE 28
+
+#endif
