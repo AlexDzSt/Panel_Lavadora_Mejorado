ee9b6ab3441a0b9695ccff02d3b9e37fca7a4392
NJR7 <2203066382@cua.uam.mx>
2024-05-27 23:38:26 -0600

    escalado correcto

--- test_calibra.py
+++ test_calibra.py
@@ -10,5 +10,5 @@ while True:
     sensor_value = sensor_pin.read_u16()
     scaled_value = sensor_value // 100  # Dividir por 64 para obtener un rango de 0 a 1023
     nivel_agua = map_value(scaled_value, 0, 600, 0, 19)  # Mapear a nivel de agua en cm de 0 a 10 cm
-    print("Nivel de agua (cm):", nivel_agua)
+    print("Nivel de agua :", nivel_agua)
     time.sleep(1)
