57eee6127907c62eb10eaf90be394a5643201836
NJR7 <2203066382@cua.uam.mx>
2024-05-27 23:18:29 -0600

    calibrando sensor2

--- Calibracion_sensor.py
+++ Calibracion_sensor.py
@@ -6,3 +6,7 @@ sensor_pin = ADC(Pin(26))  # GP26 es el pin A0 en la Pico W
 
 while True:
     # Leer el valor analógico
+    sensor_value = sensor_pin.read_u16()  # Lee el valor de 0 a 65535
+    # Convertir a un rango de 0 a 1023 similar a Arduino si es necesario
+    scaled_value = sensor_value // 64  # Dividir por 64 para obtener un rango de 0 a 1023
+    print("Nivel de agua (valor analógico):", scaled_value)
