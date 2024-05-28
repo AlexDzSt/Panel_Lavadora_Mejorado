26f6ee6fd0f07846d00861a8b96fb124a2250ba7
NJR7 <2203066382@cua.uam.mx>
2024-05-28 07:43:12 -0600

    prueba de Uart

--- test_agua.py
+++ test_agua.py
@@ -11,3 +11,18 @@ def map_value(value, in_min, in_max, out_min, out_max):
     return (value - in_min) * (out_max - out_min) // (in_max - in_min) + out_min
 
 uart_led_count = 0  # Variable para almacenar la cantidad de LEDs encendidos
+
+while True:
+# Leer datos UART
+    if uart.any():
+        uart_data = uart.read()
+        # Realizar comparación con los datos del sensor de agua
+        sensor_value = sensor_pin.read_u16()
+        scaled_value = sensor_value // 100
+        nivel_agua = map_value(scaled_value, 0, 650, 0, 10)
+        uart_led_count = int(uart_data)
+        if uart_led_count > nivel_agua:
+            print("Alerta: Más LEDs encendidos de lo esperado!")
+        else:
+            print("Todo en orden.")
+    time.sleep(1)
