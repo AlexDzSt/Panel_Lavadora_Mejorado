86c89d7682d0b405dc8f033cfd17079d3a2dad22
NJR7 <2203066382@cua.uam.mx>
2024-05-28 07:47:34 -0600

    correcion eror ValueError

--- test_agua.py
+++ test_agua.py
@@ -13,16 +13,16 @@ def map_value(value, in_min, in_max, out_min, out_max):
 uart_led_count = 0  # Variable para almacenar la cantidad de LEDs encendidos
 
 while True:
-# Leer datos UART
+    # Leer datos UART
     if uart.any():
         uart_data = uart.read()
-        # Realizar comparación con los datos del sensor de agua
-        sensor_value = sensor_pin.read_u16()
-        scaled_value = sensor_value // 100
-        nivel_agua = map_value(scaled_value, 0, 650, 0, 10)
-        uart_led_count = int(uart_data)
-        if uart_led_count > nivel_agua:
-            print("Alerta: Más LEDs encendidos de lo esperado!")
-        else:
-            print("Todo en orden.")
+        uart_data_str = uart_data.decode().strip()  # Decodificar y eliminar espacios en blanco
+        try:
+            uart_led_count = int(uart_data_str)  # Intentar convertir la cadena a un entero
+            if uart_led_count > nivel_agua:
+                print("Alerta: Más LEDs encendidos de lo esperado!")
+            else:
+                print("Todo en orden.")
+        except ValueError:  # Manejar el caso en que la cadena no sea un número entero válido
+            print("Error: La cadena recibida no es un número entero válido.")
     time.sleep(1)
