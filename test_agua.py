d97bff33aa4c824ce8090ee35574cfb492bb3578
NJR7 <2203066382@cua.uam.mx>
2024-05-28 08:14:12 -0600

    depuracion y puntocontrol

--- test_agua.py
+++ test_agua.py
@@ -13,21 +13,29 @@ def map_value(value, in_min, in_max, out_min, out_max):
 uart_led_count = 0  # Variable para almacenar la cantidad de LEDs encendidos
 
 while True:
-    # Leer datos UART
+    # Leer datos UART si están disponibles
     if uart.any():
         uart_data = uart.read()
         uart_data_str = uart_data.decode().strip()  # Decodificar y eliminar espacios en blanco
+        print(f"Datos UART recibidos: {uart_data_str}")
+
         try:
-            uart_led_count = int(uart_data_str)  # Intentar convertir la cadena a un entero
-            # Medir el nivel de agua
-            sensor_value = sensor_pin.read_u16()
-            scaled_value = sensor_value // 100
-            nivel_agua = map_value(scaled_value, 0, 650, 0, 10)
-            # Comparar el recuento de LED con el nivel de agua
-            if uart_led_count > nivel_agua:
-                print("Alerta: Más LEDs encendidos de lo esperado!")
-            else:
-                print("Todo en orden.")
+            # Extraer el número desde el string, ignorando el prefijo
+            uart_led_count = int(''.join(filter(str.isdigit, uart_data_str)))  # Intentar convertir la cadena a un entero
+            print(f"Cantidad de LEDs después de la conversión: {uart_led_count}")  # Imprimir el valor convertido
         except ValueError:  # Manejar el caso en que la cadena no sea un número entero válido
             print("Error: La cadena recibida no es un número entero válido.")
+    
+    # Medir el nivel de agua
+    sensor_value = sensor_pin.read_u16()
+    scaled_value = sensor_value // 100
+    nivel_agua = map_value(scaled_value, 0, 600, 0, 10)
+    
+    # Comparar el recuento de LED con el nivel de agua
+    print("Nivel de agua :", nivel_agua)
+    if uart_led_count < nivel_agua:
+        print("Alerta: Más agua de la requerida!")
+    else:
+        print("Todo en orden. Nivel de agua: {} (escala 0-5)".format(nivel_agua))
+    
     time.sleep(1)
