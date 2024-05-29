72f142f0a02c2c30b3d0c1eb29c260f88f276694
AlexDzSt <166785116+AlexDzSt@users.noreply.github.com>
2024-05-29 14:08:54 -0600

    Update main_panel.py

--- micropython/main_panel.py
+++ micropython/main_panel.py
@@ -1,16 +1,15 @@
-from machine import UART, Pin, I2C
+from machine import UART, Pin, I2C, ADC
 import actions
 import alarmas
 from ssd1306 import SSD1306_I2C
 from time import sleep
-
-
 
 # Configuración del UART0
 uart = UART(0, baudrate=9600, tx=Pin(12), rx=Pin(13))
 # Configuración de la pantalla OLED
 i2c = I2C(0,scl = Pin(17), sda = Pin(16), freq = 200000)
 oled = SSD1306_I2C(128, 64, i2c)
+sensor_pin = ADC(Pin(26))
 
 prev_data = None  # Inicializa prev_data fuera del bucle
 
@@ -18,11 +17,12 @@ actions.test_wifi()
 
 while True:
     if uart.any():
-        data = uart.read().decode('utf-8').strip()  # Elimina espacios en blanco alrededor del mensaje
+        data = uart.read().decode('utf-8').strip()
         if data:
             # Mostrar mensaje de bienvenida cuando enciende el sistema
             if data == "123":
                 actions.show_welcome()
+            # Mostrar mensaje de despedida cuando se apaga el sistema
             elif data == "124":
                 actions.show_bye()   
             else:
@@ -30,8 +30,11 @@ while True:
                 alarmas.play_button_beep()
                 actions.read_rotary(data)
                 
-            if data == "IN" and prev_data:
+            if data == "IN" and prev_data and actions.medir_nivel_agua(sensor_pin) == True:
                 alarmas.play_start()
                 actions.show_timer(prev_data)
+            elif data == "IN" and prev_data and actions.medir_nivel_agua(sensor_pin) == False:
+                alarmas.play_error()
+                actions.show_low_lvl()
                 
             prev_data = data
